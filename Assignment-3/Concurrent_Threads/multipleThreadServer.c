075e5c952790be901931f6c23bffaae688292a90
vboxuser <vboxuser@server.myguest.virtualbox.org>
2023-10-25 01:07:04 +0530

    Thread

--- Assignment-3/Concurrent_Threads/multipleThreadServer.c
+++ Assignment-3/Concurrent_Threads/multipleThreadServer.c
@@ -14,7 +14,9 @@
 #define ll long long
 #define PORT 8081
 
-pthread_t threads[50];
+pthread_t threads[60];
+
+
 
 ll factorial(int num){
 	ll ans = 1;
@@ -32,32 +34,64 @@ ll factorial(int num){
 }
 
 void* handle(void* clientAccRes){
-	printf("In handle...\n");
-	strcpy(readString,"");
-	ll num;
-	int clientAccRes = *((int*)clientAccRes);
-	ll recvLen = recv(clientAccRes,&readString,1024,0);
-
-	num = atoi(readString);
-
-	printf("Client message %lld\n",num);
-
-	ll ans = factorial(num);
-
+	char readString[1024];
+	int clientAcc = *((int*)clientAccRes);
 	
-	char ack[1024];
+	int i = 0;
+	while(i<50){
+		strcpy(readString,"");
+		ll num;
+		
+		if(clientAcc>=53){
+			printf("Handler----%d %d\n",clientAcc,i);
+		}
+		
+		ll recvLen = read(clientAcc,&readString,1024);
+		if(recvLen<0){
+			printf("BREAKING HERE %d\n",clientAcc);
+			close(clientAcc);
+			// i++;
+			// continue;
+			return NULL;
+		}
+
+		num = atoi(readString);
+
+		// printf("Client message %lld\n",num);
+
+		ll ans = factorial(num);
+		i++;
+		
+		char ack[1024];
+		
+		sprintf(ack,"%lld",ans);
+		ll sendLen = write(clientAcc,&ack,sizeof(ack));
+		if(sendLen <= -1){
+			printf("BREAKING HERE %d\n",clientAcc);
+			close(clientAcc);
+			return NULL;
+			// i++;
+			// continue;
+		}
+		// if(sendLen==0){
+		// 	close(clientAcc);
+		// 	return NULL;
+		// }
+		
+		// printf("Factorial is %lld\n",ans);	
+		
+	}
+	printf("Completed-----%d\n",clientAcc);
+	close(clientAcc);
 	
-	sprintf(ack,"%lld",ans);
-	ll sendLen = send(clientAccRes,&ack,sizeof(ack),0);
 	
-	printf("Factorial is %lld\n",ans);
 	return NULL;
 }
 
 int main(){
 	struct sockaddr_in server,client;
 	int clientAccRes;
-	int clientSize;
+	// int clientSize;
 
 	 // Creating the socket
 	int socketRes = socket(AF_INET,SOCK_STREAM,0);
@@ -78,33 +112,47 @@ int main(){
 		exit(1);
 	}
 
-	int listenRes = listen(socketRes,50);
+	int listenRes = listen(socketRes,60);
 	if(listenRes!=0){
 		perror("Error while listening");
 		exit(1);
 	}
 	
-
-	for(int i = 0;i<50;i++){
+	
+	int connectionCnt = 0;
+	while(connectionCnt<50){
 		
 		
-		clientSize = sizeof(client);
+		int clientSize = sizeof(client);
 	
 		clientAccRes = accept(socketRes,(struct sockaddr*)&client,&clientSize);
-		
+		printf("%d\n",clientAccRes);
 		if(clientAccRes<0){
 			perror("Error in accepting client");
 			exit(1);
-			break;
+			
 		}
 		
-		printf("Creating thread....\n");
-		pthread_create(&threads[i],NULL,handle,&clientAccRes);
-	}
+		
+		pthread_create(&threads[connectionCnt],NULL,handle,&clientAccRes);
 
+		connectionCnt++;
+		
+		// if(connectionCnt>=50){
+		// 	connectionCnt = 0;
+			
+		// }
+		
+	}
+		// close(socketRes);
+		
+		
 	for(int i = 0;i<50;i++){
 		pthread_join(threads[i],NULL);
 	}
-	printf("HELLOOOOO\n");
-	close(clientAccRes);
+
+	
+	
+	
+	close(socketRes);
 }
